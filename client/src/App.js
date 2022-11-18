7b8177817bd8232de7b300ea51f603170c235197
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2022-11-18 12:18:25 +0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StreamChat } from 'stream-chat'
+import { Chat } from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import './App.css'
+
+import { ChannelContainer, ChannelListContainer } from './components'
+
+const apiKey = 'pe8xen29kydm'
+
+const client = StreamChat.getInstance(apiKey)
+
+function App() {
+  return (
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer
+        
+        />
+
+        <ChannelContainer
+        
+        />
+
+      </Chat>
+    </div>
+  )
+}
+
+export default App
