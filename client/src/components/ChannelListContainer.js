7bdbd1fa6a58b378bc6b9ebd4ac12a69ed67560b
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2022-11-18 12:24:19 +0600

    first commit

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -43,6 +43,29 @@ function ChannelListContainer() {
               type='team'
             />
           )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              type='team'
+            />
+          )}
+        />
+
+        <ChannelList
+          filters={{}}
+          channelRenderFilterFn={() => { }}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type='messaging'
+            />
+          )}
+          Preview={(previewProps) => (
+            <TeamChannelPreview
+              {...previewProps}
+              type='messaging'
+            />
+          )}
         />
       </div>
     </>
