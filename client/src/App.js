540f0e5cd40187b79a07a34fe1ae128b0c1a33e4
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2022-11-27 21:15:59 +0600

    source code push

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 import './App.css'
+import 'stream-chat-react/dist/css/index.css'
 
 import { ChannelContainer, ChannelListContainer, Auth } from './components'
 
@@ -26,17 +27,28 @@ if (authToken) {
 }
 
 function App() {
+  const [createType, setCreateType] = useState('')
+  const [isCreating, setIsCreating] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+
   if (!authToken) return <Auth />
 
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
         <ChannelListContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
         />
 
         <ChannelContainer
-
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
         />
 
       </Chat>
